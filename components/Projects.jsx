18c78339398e80c57cbfbf20f2b2d2d034158235
rollahaideri <rohullahhaideri@gmail.com>
2023-09-11 16:35:16 +0200

    The new template for project still in progress

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import propertyImg from '../public/assets/projects/property.jpg';
+import propertyImg from '../public/assets/projects/DriveEdu.png';
 import cryptoImg from '../public/assets/projects/crypto.jpg'
 import netflixImg from '../public/assets/projects/netflix.jpg'
 import twitchImg from '../public/assets/projects/twitch.jpg'
@@ -14,13 +14,13 @@ const Projects = () => {
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
           Projects
         </p>
-        <h2 className='py-4'>What I&apos;ve Built</h2>
+
         <div className='grid md:grid-cols-2 gap-8'>
           <ProjectItem
-            title='Property Finder'
+            title='DriveEdu'
             backgroundImg={propertyImg}
             projectUrl='/property'
-            tech='React JS'
+            tech='Swift/SwiftUI'
           />
           <ProjectItem
             title='Crypto App'
